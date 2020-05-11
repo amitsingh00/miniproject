6a6e7373b3077e186bcaebd85811910edc068444
amitsingh00 <64093954+amitsingh00@users.noreply.github.com>
2020-05-11 17:25:25 +0530

    Add files via upload

---
+++ PythonprojectCSEA.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Team members :-
+# 
+# ## Amit Singh    - 2K18CSUN01009
+# ## Drishti Dua   - 2K18CSUN01060
+
+# ## Password Generator..
+
+# In[1]:
+
+
+import random
+
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = 10
+p =  "".join(random.sample(s,passlen ))
+print("The new password Generated :-")
+p
+
+
+# In[ ]:
+
+
+https://drive.google.com/drive/folders/195FDkwT0vFdbjWlOjEkouPGPEImuEH-X
+
+
+
